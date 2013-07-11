6b000e6d8c1d395d5316dbfa442489bcae22331f
Stephen Parker <ShurakaiSoft@gmail.com>
2013-07-11 13:09:15 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# web-chat
+
+## Objective
+Implement a web-based chat server, to learn about WebSockets and node's 
+Socket.IO library.
+
+This code is based on "Professional Node.js" chapter 22.
---
+++ server/app.js
@@ -0,0 +1,31 @@
+/**
+ * Web-Chat server
+ */
+var httpd = require('http').createServer(handler);
+var io = require('socket.io').listen(httpd);
+var fs = require('fs');
+
+var port = '3000';
+
+
+httpd.listen(port, function () {
+	console.log("Web Chat Server listening on port: %s", port);
+});
+
+function handler(req, res) {
+	fs.readFile(__dirname + '/index.html', function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			res.end('Error loading index.html');
+		} else {
+			res.writeHead(200);
+			res.end(data);
+		}
+	});
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.on('my event', function (content) {
+		console.log(content);
+	});
+});
